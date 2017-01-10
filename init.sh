3da25d4116eb4390702b859eaacd7bcd20d50e17
kngjle <kngjle@gmail.com>
2017-01-11 08:31:58 +0900

    add init.sh. add bash_profile

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -s ~/linux-util/etc/bash_profile ~/.bash_profile
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -s ~/linux-util/etc/vimrc ~/.vimrc
+
+ln -s ~/linux-util/etc/gitconfig ~/.gitconfig
