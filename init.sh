3cffa7c92248fd4bc551a9d4a6919659e461945f
kngjle <kngjle@gmail.com>
2020-07-14 16:49:46 +0900

    add some config

--- init.sh
+++ init.sh
@@ -2,16 +2,9 @@
 
 BASEDIR=$(cd $(dirname $0); pwd -P)
 
-curl -o $BASEDIR/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-curl -o $BASEDIR/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-
-touch $HOME/.bash_profile
-echo "# Added by linux-util" >> ~/.bash_profile
-echo "LINUX_UTIL_HOME=$BASEDIR" >> ~/.bash_profile
-echo "[ -r \"$BASEDIR/etc/bash_profile\" ] && . \"$BASEDIR/etc/bash_profile\"" >> ~/.bash_profile
-
 [ ! -e ~/.vim/bundle/Vundle.vim ] && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s $BASEDIR/etc/vimrc ~/.vimrc
+vim +PluginInstall +qall
 
 ln -s $BASEDIR/etc/gitconfig ~/.gitconfig
 
